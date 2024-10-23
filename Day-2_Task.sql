ae632f6a5e1d496ec531e0218f37b325ce0a93b6
Somendra Yadav <146230798+somendra439@users.noreply.github.com>
2024-10-23 09:40:19 +0530

    Add files via upload

---
+++ Day-2_Task.sql
@@ -0,0 +1,15 @@
+create database db_joy;
+use db_joy;
+show tables;
+CREATE TABLE Users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), balance DECIMAL(10, 2));
+INSERT INTO Users (name, balance) VALUES ('Somu', 70076.50);
+INSERT INTO Users (name, balance) VALUES ('Sonu', 63505.00);
+INSERT INTO Users (name, balance) VALUES ('Sunny', 82004.35);
+
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE Users SET balance = 6500.00 WHERE name = 'Somu';
+
+DELETE FROM Users WHERE name = 'Sunny';
+
+SELECT * FROM Users;
