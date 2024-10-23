9c91f3a0e7f2264758d77c3c555f698feed4383e
Somendra Yadav <146230798+somendra439@users.noreply.github.com>
2024-10-23 15:48:03 +0530

    Update and rename AtmSystem.java to Day_3_AtmSystem.java

--- Day_3_AtmSystem.java
+++ Day_3_AtmSystem.java
@@ -4,7 +4,7 @@ package org.example;
 import java.sql.*;
 import java.util.Scanner;
 
-public class AtmSystem
+public class Day_3_AtmSystem
 {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/db_sgvu";
     private static final String DB_USER = "root";
